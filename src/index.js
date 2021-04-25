33aedd8d17b57ab7bd2f433c8cdd1c975949428d
JuanMa Garrido <juanma.garrido@gmail.com>
2021-04-25 21:23:50 +0200

    selectors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import configureStore from './store/configureStore'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={configureStore()}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
-
+)
